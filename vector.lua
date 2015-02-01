1b699cb978eeea2f23718a9dca0e2ed0ad7030be
HarriKnox <harri.alpha96@gmail.com>
2015-01-31 22:58:47 -0800

    Added vector.isvector to check if passed values are vectors

--- vector.lua
+++ vector.lua
@@ -1,19 +1,31 @@
 local vector = {}
+local vector_meta = {}
+
+vector.isvector = function(vect)
+	return getmetatable(vect) == vector_meta
+end
 
 vector.add = function(first, second)
-	return {
-		x = first.x + second.x,
-		y = first.y + second.y,
-		z = first.z + second.z
-	}
+	if vector.isvector(first) and vector.isvector(second) then
+		-- if both passed values are vectors, then return a vector
+		return {
+			x = first.x + second.x,
+			y = first.y + second.y,
+			z = first.z + second.z
+		}
+	end
+	return first + second -- fallback
 end
 
 vector.subtract = function(first, second)
-	return {
-		x = first.x - second.x,
-		y = first.y - second.y,
-		z = first.z - second.z
-	}
+	if vector.isvector(first) and vector.isvector(second) then
+		return {
+			x = first.x - second.x,
+			y = first.y - second.y,
+			z = first.z - second.z
+		}
+	end
+	return first - second
 end
 
 return vector
