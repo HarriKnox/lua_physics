8715a271ff5a7b5c637b3b933898749bd60d4297
HarriKnox <harri.alpha96@gmail.com>
2015-02-03 23:41:43 -0800

    Shamelessly copied `incompatable` function from the Vector module

--- point.lua
+++ point.lua
@@ -10,3 +10,19 @@ type = function(thing)
 	end
 	return t
 end
+
+local incompatable = function(operation, ...)
+	local types = {...}
+	local message = string.format("incompatible type%s for point %s: ", #types == 1 and "" or "s", operation)
+	if #types < 2 then
+		message = message .. types[1]
+	elseif #types == 2 then
+		message = message .. types[1] .. " and " .. types[2]
+	else
+		for i = 1, #types - 1 do
+			message = message .. types[i] .. ", "
+		end
+		message = message .. "and " .. types[#types]
+	end
+	error(message, 3)
+end
