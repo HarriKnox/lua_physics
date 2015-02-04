94da810bb7800005e97e221b3cd31799f7295182
HarriKnox <harri.alpha96@gmail.com>
2015-02-03 23:10:31 -0800

    Added necessary closing paren

--- vector.lua
+++ vector.lua
@@ -200,7 +200,7 @@ vector.altitude = function(vect)
 		end
 		local x = vect:getx() ^ 2
 		local y = vect:gety() ^ 2
-		return math.deg(arctan(vect:getz(), math.sqrt(x + y))
+		return math.deg(arctan(vect:getz(), math.sqrt(x + y)))
 	end
 	error(geterror("calculation (altitude)", type(vect)), 2)
 end
