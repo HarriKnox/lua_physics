4c221e7da15494acfdd8f41a7bdaea775664e4f5
HarriKnox <harri.alpha96@gmail.com>
2015-02-04 10:28:46 -0800

    Moved `incompatable` into Common and changed it to `typeerror`

--- common.lua
+++ common.lua
@@ -16,3 +16,19 @@ local types = {}
 common.registertype = function(checkfunction, typename)
 	types[checkfunction] = typename
 end
+
+common.typeerror = function(typename, operation, ...)
+	local types = {...}
+	local message = string.format("incompatible type%s for %s %s: ", #types == 1 and "" or "s", typename, operation)
+	if #types < 2 then
+		message = message .. types[1]
+	elseif #types == 2 then
+		message = message .. types[1] .. " and " .. types[2]
+	else
+		for i = 1, #types - 1 do
+			message = message .. types[i] .. ", "
+		end
+		message = message .. "and " .. types[#types]
+	end
+	error(message, 3)
+end
