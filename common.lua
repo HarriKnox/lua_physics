8c83995546d2caa81e6968d98823af6f51d8f692
HarriKnox <harri.alpha96@gmail.com>
2015-02-04 10:23:15 -0800

    Added type registration to common

--- common.lua
+++ common.lua
@@ -1,1 +1,18 @@
 local common = {}
+
+local _type = type
+type = function(thing)
+	local t = _type(thing)
+	for check, name in pairs(types) do
+		if check(thing) then
+			t = name
+			break
+		end
+	end
+	return t
+end
+
+local types = {}
+common.registertype = function(checkfunction, typename)
+	types[checkfunction] = typename
+end
