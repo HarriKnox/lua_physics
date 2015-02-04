fb6143de858f2ffa9fce96d5989b45c6d3799f1a
HarriKnox <harri.alpha96@gmail.com>
2015-02-04 10:03:06 -0800

    Added Unit type and changed the module to Unit, not Units

--- units.lua
+++ units.lua
@@ -1,3 +1,12 @@
-local units = {}
-local units_meta = {}
-units_meta.__index = units_meta
+local unit = {}
+local unit_meta = {}
+unit_meta.__index = unit_meta
+
+local _type = type
+type = function(thing)
+	local t = _type(thing)
+	if t == "table" and unit.isunit(thing) then
+		t = "unit"
+	end
+	return t
+end
