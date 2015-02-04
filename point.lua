26b7ad0cb2bdf52772bf1ea23f7220ee2cfce18e
HarriKnox <harri.alpha96@gmail.com>
2015-02-03 23:38:21 -0800

    Added type registration for point

--- point.lua
+++ point.lua
@@ -1,3 +1,12 @@
 local point = {}
 local point_meta = {}
 point_meta.__index = point_meta
+
+local _type = type
+type = function(thing)
+	local t = _type(thing)
+	if t == "table" and point.ispoint(thing) then
+		t = "point"
+	end
+	return t
+end
