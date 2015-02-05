545815b75786a74335c27a0803d3c0c4f3e2fe58
HarriKnox <harri.alpha96@gmail.com>
2015-02-04 20:28:29 -0800

    Added unit creation method. The order of the SI units is mostly arbitrary; it is based somewhat on the order I write them into my physics notebook. The order goes as follows: kilogram, meter, second, ampere, kelvin, mole, candela. Contrary to SI convention, amperes and kelvin are written in this in lowercase, as opposed to the standard of Ampere and Kelvin with capital initials.

--- unit.lua
+++ unit.lua
@@ -5,7 +5,16 @@ unit_meta.__index = unit_meta
 local common = require('common')
 
 
+unit.new = function(parkg, parm, pars, para, park, parmol, parcd)
+	if type(parkg) == 'number' and type(parm) == 'number' and type(pars) == 'number' and type(para) == 'number' and type(park) == 'number' and type(parmol) == 'number' and type(parcd) == 'number' then
+		return setmetatable({kilogram = parkg, meter = parm, second = pars, ampere = para, kelvin = park, mole = parmol, candela = parcd}, unit_meta)
+	end
+	common.typeerror('unit', 'creation', type(parkg), type(parm), type(pars), type(para), type(park), type(parmol), type(parcd))
+end
+
 unit.isunit = function(un)
 	return getmetatable(un) == unit_meta
 end
 common.registertype(unit.isunit, 'unit')
+
+return unit
