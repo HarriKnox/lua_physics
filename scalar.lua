3dabb7e273bb5694c424849e3d61fd7d3abd28de
HarriKnox <harri.alpha96@gmail.com>
2015-02-06 08:47:50 -0800

    Added basic creation and type checking for scalars

--- scalar.lua
+++ scalar.lua
@@ -2,4 +2,22 @@ local scalar = {}
 local scalar_meta = {}
 scalar_meta.__index = scalar_meta
 
+local common = require('common')
+
+
+scalar.new = function(parvalue, parunits)
+	if type(parvalue) == 'number' and type(parunits) == 'unit' then
+		return setmetatable({value = parvalue, units = parunits}, scalar_meta)
+	end
+	common.typeerror('creation', parvalue, parunits, 'scalar')
+end
+
+common.setmetacall(scalar)
+
+scalar.isscalar = function(sclr)
+	return getmetatable(sclr) == scalar_meta
+end
+common.registertype(scalar.isscalar, 'scalar')
+
+
 return scalar
