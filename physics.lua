9028e4dd71be2d609a1cb5fcde1ccd0049fe1506
HarriKnox <harri.alpha96@gmail.com>
2015-02-09 09:33:32 -0800

    Added constant units to Physics

--- physics.lua
+++ physics.lua
@@ -2,3 +2,28 @@ vector = require('vector')
 point = require('point')
 unit = require('unit')
 scalar = require('scalar')
+
+local physics = {}
+
+physics.constants = {}
+physics.units = {}
+
+local kg = unit.new(1, 0, 0, 0, 0, 0, 0)
+local m = unit.new(0, 1, 0, 0, 0, 0, 0)
+local s = unit.new(0, 0, 1, 0, 0, 0, 0)
+local a = unit.new(0, 0, 0, 1, 0, 0, 0)
+local k = unit.new(0, 0, 0, 0, 1, 0, 0)
+local mol = unit.new(0, 0, 0, 0, 0, 1, 0)
+local cd = unit.new(0, 0, 0, 0, 0, 0, 1)
+
+physics.units.kilogram = kg
+physics.units.meter = m
+physics.units.second = s
+physics.units.ampere = a
+physics.units.kelvin = k
+physics.units.mole = mol
+physics.units.candela = cd
+
+physics.units.empty = units.new(0, 0, 0, 0, 0, 0, 0)
+
+return physics
