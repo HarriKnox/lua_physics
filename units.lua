9959d09ee372272ce72b5bf78065cec049753f05
HarriKnox <harri.alpha96@gmail.com>
2015-02-11 15:00:23 -0800

    Added a bunch of new SI derived units. I just followed the list on Wikipedia

--- units.lua
+++ units.lua
@@ -15,7 +15,38 @@ units.second = s
 units.ampere = a
 units.kelvin = k
 units.mole = mol
+units.candela = cdlocal units = {}
+local unit = require('unit')
+
+--[[local ]]kg = unit.new(1, 0, 0, 0, 0, 0, 0)
+--[[local ]]m = unit.new(0, 1, 0, 0, 0, 0, 0)
+--[[local ]]s = unit.new(0, 0, 1, 0, 0, 0, 0)
+--[[local ]]a = unit.new(0, 0, 0, 1, 0, 0, 0)
+--[[local ]]k = unit.new(0, 0, 0, 0, 1, 0, 0)
+--[[local ]]mol = unit.new(0, 0, 0, 0, 0, 1, 0)
+--[[local ]]cd = unit.new(0, 0, 0, 0, 0, 0, 1)
+
+units.kilogram = kg
+units.meter = m
+units.second = s
+units.ampere = a
+units.kelvin = k
+units.mole = mol
 units.candela = cd
+
+units.hertz = 1 / s
+units.newton = (kg * m) / (s ^ 2)
+units.pascal = units.newton / (m ^ 2)
+units.joule = units.newton * m
+units.watt = units.joule / s
+units.coulomb = s * a
+units.volt = units.watt / a
+units.farad = units.coulomb / units.volt
+units.ohm = units.volt / a
+
+units.empty = unit.new(0, 0, 0, 0, 0, 0, 0)
+
+return units
 
 units.empty = unit.new(0, 0, 0, 0, 0, 0, 0)
 
