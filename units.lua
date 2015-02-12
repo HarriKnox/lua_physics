0a746493e2afa781cd1f75396be51e29e1c936aa
HarriKnox <harri.alpha96@gmail.com>
2015-02-11 22:25:37 -0800

    Localized all the base units again in Units

--- units.lua
+++ units.lua
@@ -1,13 +1,13 @@
 local units = {}
 local unit = require('unit')
 
---[[local ]]kg = unit.new(1, 0, 0, 0, 0, 0, 0)
---[[local ]]m = unit.new(0, 1, 0, 0, 0, 0, 0)
---[[local ]]s = unit.new(0, 0, 1, 0, 0, 0, 0)
---[[local ]]a = unit.new(0, 0, 0, 1, 0, 0, 0)
---[[local ]]k = unit.new(0, 0, 0, 0, 1, 0, 0)
---[[local ]]mol = unit.new(0, 0, 0, 0, 0, 1, 0)
---[[local ]]cd = unit.new(0, 0, 0, 0, 0, 0, 1)
+local kg = unit.new(1, 0, 0, 0, 0, 0, 0)
+local m = unit.new(0, 1, 0, 0, 0, 0, 0)
+local s = unit.new(0, 0, 1, 0, 0, 0, 0)
+local a = unit.new(0, 0, 0, 1, 0, 0, 0)
+local k = unit.new(0, 0, 0, 0, 1, 0, 0)
+local mol = unit.new(0, 0, 0, 0, 0, 1, 0)
+local cd = unit.new(0, 0, 0, 0, 0, 0, 1)
 
 units.kilogram = kg
 units.meter = m
