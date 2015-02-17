ac53a9149ce0fcaee38443f98e5d1dbcc01726f9
HarriKnox <harri.alpha96@gmail.com>
2015-02-17 09:51:07 -0800

    Added kinematics.position to return 1 dimensional position of a point given initial position, initial velocity, constant acceleration, and the time elapsed.

--- kinematics.lua
+++ kinematics.lua
@@ -1,3 +1,21 @@
 local kinematics = {}
+local common = require('common')
+
+kinematics.position = function(initialposition, initialvelocity, acceleration, timeelapsed)
+	if common.alloftype(initialposition, initialvelocity, acceleration, timeelapsed, 'scalar') then
+		local units = require('units')
+		local meter = units.meter
+		local speed = meter / units.second
+		local accel = speed / units.second
+		if initialposition.units == meter and
+				initialvelocity.units == speed and
+				acceleration.units == accel and
+				timeelapsed.units == units.second then
+			return initialposition + (initialvelocity * timeelapsed) + (acceleration * (timeelapsed ^ 2) / 2)
+		end
+		error("Incompatable types", 2)
+	end
+	common.typeerror('position', initialposition, initialvelocity, acceleration, timeelapsed, 'kinematics')
+end
 
 return kinematics
