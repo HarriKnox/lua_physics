ae9f8f799d4d9276fb83f751c34dab85c82072d9
HarriKnox <harri.alpha96@gmail.com>
2015-02-26 22:40:04 -0800

    Added a bunch of different unit types

--- units.lua
+++ units.lua
@@ -11,6 +11,7 @@ local cd = unit.new(0, 0, 0, 0, 0, 0, 1)
 
 units.kilogram = kg
 units.meter = m
+units.metre = m -- For the non-Americans
 units.second = s
 units.ampere = a
 units.kelvin = k
@@ -26,6 +27,17 @@ units.coulomb = s * a
 units.volt = units.watt / a
 units.farad = units.coulomb / units.volt
 units.ohm = units.volt / a
+
+units.minute = 60 * s
+units.hour = 60 * units.minute
+units.day = 24 * units.hour
+units.hectare = 10000 * (m ^ 2)
+units.liter = 0.001 * (m ^ 3)
+units.litre = units.liter -- For the non-Americans
+units.tonne = 1000 * kg
+units.electronvolt = 1.6021765314e-19 * units.joule
+units.astronomicalunit = 1.495978706916e11 * units.meter
+units.speedoflight = 299792458 * m / s
 
 units.empty = unit.new(0, 0, 0, 0, 0, 0, 0)
 
