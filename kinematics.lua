fa2a1a26fe4abc81ec41992ff2df109a8ac976d6
HarriKnox <harri.alpha96@gmail.com>
2015-03-02 09:05:12 -0800

    Added timeelapsed to get time elapsed given a change in position, initial velocity, and constant acceleration

--- kinematics.lua
+++ kinematics.lua
@@ -1,6 +1,15 @@
 local kinematics = {}
 local common = require('common')
 
+--[[
+	x_f = x_i + (v)(t) + (a)(t)/2
+	t = (-v +- (v^2 - 4(a)(d_x))^0.5) / 2(a)
+	v_f = v_i + (a)(t)
+	2(a)(d_x) = (v_f)^2 - (v_i)^2
+	v_ave = (v_f - v_i) / 2
+	a_ave = v_ave / t
+--]]
+
 kinematics.position = function(initialposition, initialvelocity, acceleration, timeelapsed)
 	if type(timeelapsed) == 'quantity' and common.alloftype(initialposition, initialvelocity, acceleration, {'quantity', 'vector'}) then
 		local units = require('units')
@@ -23,12 +32,33 @@ kinematics.velocity = function(initialvelocity, acceleration, timeelapsed)
 		local units = require('units')
 		local speed = units.meter / units.second
 		local accel = speed / units.second
-		if initialvelocity.units == speed and acceleration.units = accel and timeelapsed.units == units.second then
+		if initialvelocity.units == speed and acceleration.units == accel and timeelapsed.units == units.second then
 			return initialvelocity + (acceleration * timeelapsed)
 		end
 		common.uniterror('velocity', initialvelocity, acceleration, timeelapsed, 'kinematics')
 	end
 	common.typeerror('velocity', initialvelocity, acceleration, timeelapsed, 'kinematics')
+end
+
+kinematics.timeelapsed = function(deltax, velocity, acceleration)
+	if common.alloftype(deltax, velocity, acceleration, {'quantity', 'vector'}) then
+		local units = require('units')
+		local speed = units.meter / units.second
+		local accel = speed / units.second
+		if delta_x.units == units.meter and velocity.units == speed and acceleration.units == accel then
+			local discriminant = (velocity ^ 2) - (4 * acceleration * deltax)
+			if discriminant < 0 then
+				return nil
+			elseif discriminant == 0 then
+				return (-velocity) / (2 * acceleration)
+			end
+			local plus = ((-velocity) + math.sqrt(discriminant)) / (2 * acceleration)
+			local minus = ((-velocity) - math.sqrt(discriminant)) / (2 * acceleration)
+			return plus, minus
+		end
+		common.uniterror('timeelapsed', deltax, velocity, acceleration, 'kinematics')
+	end
+	common.typeerror('timeelapsed', deltax, velocity, acceleration, 'kinematics')
 end
 
 return kinematics
