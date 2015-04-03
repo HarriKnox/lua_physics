9bf0d62930767ec5d7eef245622aa2cb1443dc93
HarriKnox <harri.alpha96@gmail.com>
2015-04-02 23:16:55 -0700

    Added `particle.tostring` and the rest of the metamethods

--- particle.lua
+++ particle.lua
@@ -32,3 +32,35 @@ particle.equals = function(first, second)
 	end
 	return false
 end
+
+particle.tostring = function(part, comp)
+	return "Particle: " .. part.mass:tostring() .. " at " .. part.position:tostring(comp)
+end
+
+
+common.getmethods(particle, particle_meta)
+
+particle_meta.__eq = particle.equals
+particle_meta.__tostring = particle.tostring
+
+particle_meta.__add = common.notsupported('particles', 'addition')
+particle_meta.__sub = common.notsupported('particles', 'subtraction')
+particle_meta.__mul = common.notsupported('particles', 'multiplication')
+particle_meta.__div = common.notsupported('particles', 'division')
+particle_meta.__unm = common.notsupported('particles', 'unary-minus')
+particle_meta.__idiv = common.notsupported('particles', 'int-divide')
+particle_meta.__len = common.notsupported('particles', 'length')
+particle_meta.__mod = common.notsupported('particles', 'modulo')
+particle_meta.__pow = common.notsupported('particles', 'powers')
+particle_meta.__concat = common.notsupported('particles', 'concatination')
+particle_meta.__lt = common.notsupported('particles', 'less-than')
+particle_meta.__le = common.notsupported('particles', 'less-than-or-equal-to')
+particle_meta.__band = common.notsupported('particles', 'bitwise')
+particle_meta.__bor = particle_meta.__band
+particle_meta.__bxor = particle_meta.__band
+particle_meta.__bnot = particle_meta.__band
+particle_meta.__shl = particle_meta.__band
+particle_meta.__shr = particle_meta.__band
+
+
+return particle
