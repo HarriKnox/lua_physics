d00fb32b8596a50808d0f01d5f547aace9647182
HarriKnox <harri.alpha96@gmail.com>
2015-04-02 22:19:34 -0700

    Added `particle.new`

--- particle.lua
+++ particle.lua
@@ -3,3 +3,11 @@ local particle_meta = {}
 particle_meta.__index = particle_meta
 local common = require('common')
 
+
+particle.new = function(parm, parp)
+	local units = require('units')
+	if type(parm) == 'scalar' and parm.units == units.kilogram and common.istype(parp, {'scalar', 'vector'}) and parp.units == units.meter then
+		return setmetatable({mass = parm, position = parp}, particle_meta)
+	end
+	common.errortype('creation', parm, parp, 'particle')
+end
