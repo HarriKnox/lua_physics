6e68981e40ce5c8f014ff836838c12dff14a9ae6
HarriKnox <harri.alpha96@gmail.com>
2015-04-12 20:17:39 -0700

    Added `System.addparticle` to add an object to the System objects list if that exact particle is not already in it. There can be clones, but you can't put copies.

--- system.lua
+++ system.lua
@@ -9,3 +9,14 @@ end
 
 common.setmetacall(system)
 common.registertype(system_meta, 'system')
+
+system.addparticle(sys, part)
+	if type(sys) == 'system' and type(part) == 'particle' then
+		if not common.intable(part, sys.objects) then
+			table.insert(sys.objects, part)
+			return sys
+		end
+		error("particle already exists in system")
+	end
+	common.typeerror('particle addition', sys, part, 'system')
+end
