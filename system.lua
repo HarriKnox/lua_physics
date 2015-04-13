c7d9a51aa341e97907b6feac025e4aca61d6a94b
HarriKnox <harri.alpha96@gmail.com>
2015-04-12 17:37:48 -0700

    Added System `new`, metacall, and type

--- system.lua
+++ system.lua
@@ -2,3 +2,10 @@ local system = {}
 local system_meta = {}
 system_meta.__index = system_meta
 local common = require('common')
+
+system.new = function()
+	return setmetatable({objects = {}}, system_meta)
+end
+
+common.setmetacall(system)
+common.registertype(system_meta, 'system')
