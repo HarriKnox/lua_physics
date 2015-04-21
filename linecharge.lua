67e69ff0b0a985cf670a572a3421e9ec6f850c70
HarriKnox <harri.alpha96@gmail.com>
2015-04-20 17:39:54 -0700

    Added `linecharge.new` which takes a charge, mass, endpoint, and direction

--- linecharge.lua
+++ linecharge.lua
@@ -3,3 +3,10 @@ local linecharge_meta = {}
 linecharge_meta.__index = linecharge_meta
 local common = require('common')
 
+linecharge.new = function(parc, parm, parp, pard)
+	local units = require('units')
+	if type(parc) == 'quantity' and parc.units == units.coulomb type(parm) == 'quantity' and parm.units == units.kilogram and type(parp) == 'vector' and parp.units == units.meter and type(pard) == 'vector' and pard.units == units.meter then
+		return setmetatable({charge = parc, mass = parm, point = parp, direction = pard}, linecharge_meta)
+	end
+	common.typeerror('creation', parc, parm, parp, pard, 'linecharge')
+end
